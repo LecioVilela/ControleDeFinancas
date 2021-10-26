cab3009c941500d7c522404f46bef638a094e14f
LecioVilela <lecio.vilela@yahoo.com>
2021-10-25 22:08:18 -0300

    update project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AtualizarCategoriaComponent } from './components/Categoria/atualizar-categoria/atualizar-categoria.component';
+import { ListagemCategoriasComponent } from './components/Categoria/listagem-categorias/listagem-categorias.component';
+import { NovaCategoriaComponent } from './components/Categoria/nova-categoria/nova-categoria.component';
+
+const routes: Routes = [
+  {
+    path : 'categorias/listagemcategorias', component: ListagemCategoriasComponent
+  },
+
+  {
+    path : 'categorias/novacategoria', component: NovaCategoriaComponent
+  },
+
+  {
+    path : 'categorias/atualizarcategoria/:id', component: AtualizarCategoriaComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
